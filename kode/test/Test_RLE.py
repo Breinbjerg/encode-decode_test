f57d200bffc27d40fede29f4240d3f2717976e53
Steffen Breinbjerg <steffenpb@outlook.dk>
2019-09-25 09:30:18 +0200

    Added test encode 14 for pytest

--- kode/test/Test_RLE.py
+++ kode/test/Test_RLE.py
@@ -30,7 +30,7 @@ def test_rl11():
     assert rle_decode('5a6c6b7g') == 'aaaaaccccccbbbbbbggggggg'
     
 def test_rl14():
-    assert rle("hhhtttfff") == 'hhtttfff'
+    assert rle("hhhtttfff") == 'hhtttfffg'
     
     
 
