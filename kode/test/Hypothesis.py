3113601f9f13f592188eea0090c23623897c9732
Steffen Breinbjerg <steffenpb@outlook.dk>
2020-01-06 11:41:45 +0100

    Changes to test_rle

--- kode/test/Hypothesis.py
+++ kode/test/Hypothesis.py
@@ -4,9 +4,9 @@ Created on 20 Sep 2019
 @author: Steffen
 '''
 import sys
-sys.path.append('/home/Steffen/rle_encode/encode-decode_test/kode')
+sys.path.append('/home/Steffen/rle_encode/encode-decode_test/kode/')
 
-
+import pytest 
 from RLE import rle_decode, rle
 from hypothesis import given
 from hypothesis.strategies import text, characters
@@ -14,7 +14,9 @@ from hypothesis.strategies import text, characters
 
 
 
-@given(text(characters('L',('P','S','C'),('ª','µ')),1,3))
+
+
+@given(text(characters(whitelist_categories=['Ll','Lu'],blacklist_categories=['So','Sk','Sm','Sc'],min_codepoint=65,max_codepoint=122),0,10))
 def hypo_rle(mess):
     assert(rle_decode(rle(mess)))
     
--- kode/test/test_rle.py
+++ kode/test/test_rle.py
@@ -4,38 +4,38 @@ Created on 11 Sep 2019
 @author: Steffen
 '''
 import sys
-sys.path.append('/home/Steffen/rle_encode/encode-decode_test/kode')
+sys.path.append('/home/Steffen/rle_encode/encode-decode_test/kode/')
 
-
+import pytest
 from RLE import rle_decode, rle
 
 
 
 def test_rl4():
     assert rle_decode('3a4b') == 'aaabbbb'
-    
+
 # fail input - There must always be a number before character.
 def test_rl5():
     assert  rle_decode('1a5b6c') == 'abbbbbcccccc'
-    
+
 def test_rl6():
-    assert rle_decode('15a3b') == 'aaaaaaaaaaaaaaabbb'
+    assert rle_decode('13a3b') == 'aaaaaaaaaaaaaaabbb'
 
 def test_rl7():
     assert rle_decode('110a30b') == 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
-    
+
 def test_rl9():
     assert rle_decode('') == ''
 
 def test_rl10():
     assert rle('aaaaaccccccbbbbbbggggggg') == '5a6c6b7g'
-    
+
 def test_rl11():
     assert rle_decode('5a6c6b7g') == 'aaaaaccccccbbbbbbggggggg'
-    
+
 def test_rl14():
     assert rle("hhhtttfff") == '3h3t3f'
-    
-    
 
-        
+
+
+
